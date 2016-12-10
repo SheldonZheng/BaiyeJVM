5f03912c197a57dde9bef6d982b881de76638194
Baiye <zhenghangtxdyr@gmail.com>
2016-12-10 10:57:22 +0800

    加入Mul算术指令;

---
+++ src/instructions/math/mul.go
@@ -0,0 +1,50 @@
+package math
+
+import (
+	"instructions/base"
+	"rtda"
+)
+
+//Double
+type DMUL struct{ base.NoOperandsInstruction }
+
+func (self *DMUL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopDouble()
+	v1 := stack.PopDouble()
+	result := v1 * v2
+	stack.PushDouble(result)
+}
+
+//Flat
+type FMUL struct{ base.NoOperandsInstruction }
+
+func (self *FMUL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := v1 * v2
+	stack.PushFloat(result)
+}
+
+//Int
+type IMUL struct{ base.NoOperandsInstruction }
+
+func (self *IMUL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 * v2
+	stack.PushInt(result)
+}
+
+//Long
+type LMUL struct{ base.NoOperandsInstruction }
+
+func (self *LMUL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 * v2
+	stack.PushLong(result)
+}

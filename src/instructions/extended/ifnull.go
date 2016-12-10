4f85c491a500be9eb6dd6c9aa83da0159372c92e
Baiye <zhenghangtxdyr@gmail.com>
2016-12-10 11:32:30 +0800

    加入IFNULL,IFNONNULL拓展指令;

---
+++ src/instructions/extended/ifnull.go
@@ -0,0 +1,25 @@
+package extended
+
+import (
+	"instructions/base"
+	"rtda"
+)
+
+type IFNULL struct{ base.BranchInstruction }
+
+func (self *IFNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref == nil {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+// Branch if reference not null
+type IFNONNULL struct{ base.BranchInstruction }
+
+func (self *IFNONNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref != nil {
+		base.Branch(frame, self.Offset)
+	}
+}

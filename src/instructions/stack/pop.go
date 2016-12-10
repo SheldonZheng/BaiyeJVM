1e714bf75db7f06c1d49abb0ed9879a6ae78943c
Baiye <zhenghangtxdyr@gmail.com>
2016-12-10 10:29:10 +0800

    加入Pop栈指令;

---
+++ src/instructions/stack/pop.go
@@ -0,0 +1,23 @@
+package stack
+
+import (
+	"instructions/base"
+	"rtda"
+)
+
+type POP struct{ base.NoOperandsInstruction }
+
+type POP2 struct{ base.NoOperandsInstruction }
+
+//弹出int、float等占用一个位置的变量
+func (self *POP) Execute(frame *rtda.Frame)  {
+	stack := frame.OperandStack()
+	stack.PopSlot()
+}
+
+//两个
+func (self *POP2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	stack.PopSlot()
+	stack.PopSlot()
+}

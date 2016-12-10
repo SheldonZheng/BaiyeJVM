92643ddb3d33105bab57188a352a479fddd6bc8d
Baiye <zhenghangtxdyr@gmail.com>
2016-12-10 11:16:36 +0800

    加入if_acmp<Cond>运算指令;

---
+++ src/instructions/comparisons/if_acmp.go
@@ -0,0 +1,31 @@
+package comparisons
+
+import (
+	"instructions/base"
+	"rtda"
+)
+
+//Object
+type IF_ACMPEQ struct{ base.BranchInstruction }
+
+func (self *IF_ACMPEQ) Execute(frame *rtda.Frame) {
+	if _acmp(frame) {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IF_ACMPNE struct{ base.BranchInstruction }
+
+func (self *IF_ACMPNE) Execute(frame *rtda.Frame) {
+	if !_acmp(frame) {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func _acmp(frame *rtda.Frame) bool {
+	stack := frame.OperandStack()
+	ref2 := stack.PopRef()
+	ref1 := stack.PopRef()
+	//TODO 调用Equals?
+	return ref1 == ref2
+}

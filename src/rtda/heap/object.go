77df1c2e1d7401464cb0d454f5eee6f982c17de9
Baiye <zhenghangtxdyr@gmail.com>
2016-12-17 10:38:38 +0800

    Definition "PutField" Instruction;

--- src/rtda/heap/object.go
+++ src/rtda/heap/object.go
@@ -11,3 +11,14 @@ func newObject(class *Class) *Object {
 		fields: newSlots(class.instanceSlotCount),
 	}
 }
+
+func (self *Object) Class() *Class {
+	return self.class
+}
+func (self *Object) Fields() Slots {
+	return self.fields
+}
+
+func (self *Object) IsInstanceOf(class *Class) bool {
+	return class.isAssignableFrom(self.class)
+}

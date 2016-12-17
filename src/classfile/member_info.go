5e2b6694a146918ccee20981cb7649eff660f001
Baiye <zhenghangtxdyr@gmail.com>
2016-12-17 10:11:21 +0800

    Definition Class Loader;

--- src/classfile/member_info.go
+++ src/classfile/member_info.go
@@ -49,3 +49,13 @@ func (self *MemberInfo) AccessFlags() uint16 {
 	return self.accessFlags
 }
 
+func (self *MemberInfo) ConstantValueAttribute() *ConstantValueAttribute {
+	for _, attrInfo := range self.attributes {
+		switch attrInfo.(type) {
+		case *ConstantValueAttribute:
+			return attrInfo.(*ConstantValueAttribute)
+		}
+	}
+	return nil
+}
+
--- src/rtda/heap/cp_interface_methodref.go
+++ src/rtda/heap/cp_interface_methodref.go
@@ -6,6 +6,7 @@ type InterfaceMethodRef struct {
 	MemberRef
 	method *Method
 }
+
 func newInterfaceMethodRef(cp *ConstantPool,
 refInfo *classfile.ConstantInterfaceMethodrefInfo) *InterfaceMethodRef {
 	ref := &InterfaceMethodRef{}

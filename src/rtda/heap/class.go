f29df296b0d035947b4e95774f3f629cd903e10e
Baiye <zhenghangtxdyr@gmail.com>
2017-01-05 16:03:06 +0800

    Implement "System.arraycopy()"

--- src/rtda/heap/class.go
+++ src/rtda/heap/class.go
@@ -163,3 +163,8 @@ func (self *Class) getField(name, descriptor string, isStatic bool) *Field {
 func (self *Class) JavaName() string {
 	return strings.Replace(self.name, "/", ".", -1)
 }
+
+func (self *Class) IsPrimitive() bool {
+	_, ok := primitiveTypes[self.name]
+	return ok
+}

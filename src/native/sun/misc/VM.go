78139ae87a6f8717e7b5715eff451e23ac3255f5
Baiye <zhenghangtxdyr@gmail.com>
2017-01-11 16:07:14 +0800

    Add "sun/misc/*"

--- src/native/sun/misc/VM.go
+++ src/native/sun/misc/VM.go
@@ -1,11 +1,8 @@
 package misc
 
-import (
-	"instructions/base"
-	"native"
-	"rtda"
-	"rtda/heap"
-)
+import "instructions/base"
+import "native"
+import "rtda"
 
 func init() {
 	native.Register("sun/misc/VM", "initialize", "()V", initialize)
@@ -19,20 +16,3 @@ func initialize(frame *rtda.Frame) {
 	initSysClass := jlSysClass.GetStaticMethod("initializeSystemClass", "()V")
 	base.InvokeMethod(frame, initSysClass)
 }
-/*
-func initialize(frame *rtda.Frame) {
-	vmClass := frame.Method().Class()
-	savedProps := vmClass.GetRefVar("savedProps", "Ljava/util/Properties;")
-	key := heap.JString(vmClass.Loader(), "foo")
-	val := heap.JString(vmClass.Loader(), "bar")
-
-	frame.OperandStack().PushRef(savedProps)
-	frame.OperandStack().PushRef(key)
-	frame.OperandStack().PushRef(val)
-
-	propsClass := vmClass.Loader().LoadClass("java/util/Properties")
-	setPropMethod := propsClass.GetInstanceMethod("setProperty",
-		"(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Object;")
-	base.InvokeMethod(frame, setPropMethod)
-}
-*/
